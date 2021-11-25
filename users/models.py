ee48bca3c2876a824b3c5775d767fc11f8a98002
rotemhadad <rotem6432@gmail.com>
2021-11-25 19:30:14 +0200

    teacher add

--- users/models.py
+++ users/models.py
@@ -3,20 +3,26 @@ from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
-class Student(models.Model):
-    name = models.CharField(max_length=200, null = True)
-    student_id = models.CharField(max_length=200,null = True)
-    status = models.BooleanField(null= True)
-    def __str__(self):
-        return f'Name: {self.name}, ID: {self.student_id}'
-
-
 class Manager(models.Model):
-    student = models.ForeignKey(Student,on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    Manager_id = models.CharField(max_length=200,null = True)
+    name = models.CharField(max_length=200, null = True)
+    user_id = models.CharField(max_length=200,null = True)
     status = models.BooleanField(null= True)
-
     def __str__(self):
         return f'Name: {self.name}, ID: {self.Manager_id}'
 
+class Teacher(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    user_id = models.CharField(max_length=200, null = True)
+    status = models.BooleanField(null= True)
+    manager = models.ForeignKey(Manager, on_delete = models.CASCADE)
+    def __str__(self):
+        return f'Name: {self.name}, ID: {self.Teacher_id}'
+
+class Student(models.Model):
+    name = models.CharField(max_length=200, null = True)
+    user_id = models.CharField(max_length=200,null = True)
+    status = models.BooleanField(null= True)
+    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE)
+    manager = models.ForeignKey(Manager, on_delete = models.CASCADE)
+    def __str__(self):
+        return f'Name: {self.name}, ID: {self.student_id}'
