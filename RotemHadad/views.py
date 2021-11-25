6cf2372b6ed7c25621281e7c35143babaa526eb8
rotemhadad <rotem6432@gmail.com>
2021-11-25 19:03:36 +0200

    start now

--- RotemHadad/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def say_myname(request):
-    #return HttpResponse(' Hello! my name is Rotem ')
-    return render(request,'hello.html', {'name': 'Rotem'})
-
-
