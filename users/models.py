68b39573aa116d7cc2453edb20572c0bdca4c0d0
rotemhadad <rotem6432@gmail.com>
2021-11-25 18:21:31 +0200

    comment line

--- users/models.py
+++ users/models.py
@@ -12,7 +12,7 @@ class Student(models.Model):
 
 
 class Manager(models.Model):
-    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    #student = models.ForeignKey(Student,on_delete=models.CASCADE)
 
     name = models.CharField(max_length=200)
     Manager_id = models.CharField(max_length=200,null = True)
