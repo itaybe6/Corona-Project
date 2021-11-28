5027ec01e9d97f66813a529242e5cee1c2479f61
rotemhadad <rotem6432@gmail.com>
2021-11-28 10:43:57 +0200

    with return html

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-#from .viwes import NAME_FUNCTIONS....
+from . import views
 from django.conf.urls import url
 
-app name = 'user'
+
+appname = 'user'
 
 urlpatterns = [
     path(r'', views.get_index), # ----/users/
-    path('manager/login.html',log_in_manager,name ='login-manager')
+    path('manager/login',views.log_in_manager)
 
 
 ]
