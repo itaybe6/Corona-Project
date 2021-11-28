ddb8dce24c31fba9f30ce9f2dbf494f223f97e29
rotemhadad <rotem6432@gmail.com>
2021-11-28 18:59:50 +0200

    homepage fix

--- users/urls.py
+++ users/urls.py
@@ -7,10 +7,11 @@ appname = 'users'
 
 urlpatterns = [
     path(r'', views.get_index), # ----/users/
-    path('manager/sign-up',views.signup_manager, name = 'sign-up-manager'),
     path('chooseprofile',views.get_chooseprofile, name = 'chooseprofile'),
-    path('student/sign-up',views.get_student_signup, name = 'student_signup'),
-    path('teacher/sign-up',views.get_teacher_signup),
+    path('student/signup',views.get_student_signup, name = 'student_signup'),
+    path('teacher/signup',views.get_teacher_signup,name = 'teacher_signup'),
+    path('manager/signup',views.get_manager_signup, name = 'manager_signup'),
+
 
 
 
