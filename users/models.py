3d75f53cd72a9096a82ad215309714bce866cbca
itaybe6 <itaybenyair99@gmail.com>
2021-11-28 13:08:36 +0200

    add signup Mnager

--- users/models.py
+++ users/models.py
@@ -4,12 +4,12 @@ from django.db.models.deletion import CASCADE
 # Create your models here.
 
 class Manager(models.Model):
-    name = models.CharField(max_length=200, null = True)
+    name = models.CharField(max_length=200, null = True, default = None)
     user_id = models.CharField(max_length=200,null = True)
     status = models.BooleanField(null= True,default = True)
-    phone_number = models.CharField(max_length=200, null=True)
-    password = models.CharField(max_length=200, null=True)
-    school = models.CharField(max_length=200, null=True)
+    phone_number = models.CharField(max_length=200, null=True, default = None)
+    password = models.CharField(max_length=200, null=True, default = None)
+    school = models.CharField(max_length=200, null=True, default = None)
 
     def __str__(self):
         return f'Name: {self.name}, ID: {self.user_id}'
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse ,Http404
 from users.models import Manager,Teacher,Student
 # Create your views here.
 
@@ -7,12 +7,21 @@ def get_index(request):
     return HttpResponse('users index')
     #return render(request,'hello.html')
 
-def login_manager(request):
-    name = request.Post['name']
-    ID = request.Post['userName']
-    password = request.Post['password']
-    phone_num = request.Post['phone_number']
-    school = request.Post['school']
 
+def login_manager(request):
+
+    user_id = request.Post['user_id']
+    try :
+        manager = Manager.objects.get(user_id = user_id) 
+    except Manager.DoesNotExist:
+        raise Http404('מנהל לא קיים במערכת. אנא פנה למנהל המערכת')
+    name = request.Post['name']
+    phone_number = request.Post['phone_number']
+    school = request.Post['school']
+    password = request.Post['psw']
+    Manager.objects.get(user_id = user_id).delete() #delete the object that we created earlier
     manager = Manager(name = name,password = password,user_id = ID, phone_number = phone_num,school=school)
     manager.save()
+    return render(request,'homepage/index.html')
+
+
