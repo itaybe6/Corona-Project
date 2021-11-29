afd8543e281c2844c9fbff662f46641352d71eb5
IsraeLasrY <93082806+IsraeLasrY@users.noreply.github.com>
2021-11-29 12:24:55 +0200

    unit testing

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,21 @@
+import unittest
+from django.db import models
+from django.db.models import manager
 from django.test import TestCase
+from users.models import Manager
+from users.models import Teacher
+from users.models import Student
 
+
+class CreateUserTastCase(unittest.TestCase):
+
+    def create_manager(self):
+        """if we can create manager user for school"""
+        manager = Manager.user_id(123456789)
+
+        self.assertTrue(Manager.user_id(123456789, manager))
+
+
+if __name__ == '__main__':
+    unittest.main()
 # Create your tests here.
