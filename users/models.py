a1ecb03a2f8012b7fefac4a14cd60d818627126a
IsraeLasrY <93082806+IsraeLasrY@users.noreply.github.com>
2021-12-13 18:56:34 +0200

    changes

--- users/models.py
+++ users/models.py
@@ -3,43 +3,37 @@ from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
-
 class Manager(models.Model):
-    name = models.CharField(max_length=200, null=True, default=None)
-    user_id = models.CharField(max_length=200, null=True)
-    status = models.BooleanField(null=True, default=True)
-    phone_number = models.CharField(max_length=200, null=True, default=None)
-    password = models.CharField(max_length=200, null=True, default=None)
-    school = models.CharField(max_length=200, null=True, default=None)
+    name = models.CharField(max_length=200, null = True, default = None)
+    user_id = models.CharField(max_length=200,null = True)
+    status = models.BooleanField(null= True,default = True)
+    phone_number = models.CharField(max_length=200, null=True, default = None)
+    password = models.CharField(max_length=200, null=True, default = None)
+    school = models.CharField(max_length=200, null=True, default = None)
 
     def __str__(self):
         return f'Name: {self.name}, ID: {self.user_id}'
-
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=200, null=True, default=None)
-    user_id = models.CharField(max_length=200, null=True)
-    status = models.BooleanField(null=True, default=True)
-    phone_number = models.CharField(max_length=200, null=True, default=None)
-    password = models.CharField(max_length=200, null=True, default=None)
-    my_class = models.CharField(max_length=200, null=True, default=None)
-    manager = models.ForeignKey(
-        Manager, on_delete=models.CASCADE, default=None)
+    name = models.CharField(max_length=200, null=True , default = None )
+    user_id = models.CharField(max_length=200, null = True)
+    status = models.BooleanField(null= True,default = True)
+    phone_number = models.CharField(max_length=200, null=True,default = None)
+    password = models.CharField(max_length=200, null=True,default = None)
+    my_class= models.CharField(max_length=200, null=True,default = None)
+    manager = models.ForeignKey(Manager, on_delete = models.CASCADE,default = None)
 
     def __str__(self):
         return f'Name: {self.name}, ID: {self.user_id}'
 
-
 class Student(models.Model):
-    name = models.CharField(max_length=200, null=True, default=None)
-    user_id = models.CharField(max_length=200, null=True)
-    status = models.BooleanField(null=True, default=False)
-    phone_number = models.CharField(max_length=200, null=True, default=None)
-    password = models.CharField(max_length=200, null=True, default=None)
-    teacher = models.ForeignKey(
-        Teacher, on_delete=models.CASCADE, default=None)
-    manager = models.ForeignKey(
-        Manager, on_delete=models.CASCADE, default=None)
+    name = models.CharField(max_length=200, null = True,default = None)
+    user_id = models.CharField(max_length=200,null = True)
+    status = models.BooleanField(null= True , default = False)
+    phone_number = models.CharField(max_length=200, null=True,default = None)
+    password = models.CharField(max_length=200, null=True,default = None)
+    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE,default = None)
+    manager = models.ForeignKey(Manager, on_delete = models.CASCADE,default = None)
+    def __str__(self):
+        return f'Name: {self.name}, ID: {self.user_id}'
 
-    """def __str__(self):
-        return f'Name: {self.name}, ID: {self.user_id}"""
--- users/urls.py
+++ users/urls.py
@@ -17,15 +17,22 @@ urlpatterns = [
     path(r'teacher/signup_success',views.submit_Teacher, name = 'submit_Teacher'),
     path(r'student/signup_success',views.submit_Student, name = 'submit_Student'),
     
+
     path(r'Home',views.Conect,name = 'Conect'),
-    path('Phones/897<int:user_id>654',views.Phones),
-    path('manager/PhonesT/<int:user_id>',views.PhonesTeacher,name = 'PhonesT'),
+    path('Phones/897<int:user_id>654/',views.Phones),
+    path('PhonesT/897<int:user_id>654',views.PhonesTeacher,name = 'PhonesT'),
+    path('PhoneStu/897<int:user_id>654',views.PhonesStudent, name = 'PhoneStu'),
 
 
+    #Home page between paths on the site
+    path(r'HomePageTeacher/897<int:user_id>654/',views.HomePageBetweenPathTeacher,name = 'HomePageTeacher'),
+    path(r'HomePageManager/897<int:user_id>654/',views.HomePageBetweenPathManager,name = 'HomePageManager'),
+    path(r'HomePageStudent/897<int:user_id>654/',views.HomePageBetweenPathStudent, name = 'HomePageStudent'),
 
 
-
-
+    path('ChanageStatusStudent/897<int:user_id>654/',views.ChanageStatusStudent , name = 'ChanageStatusStudent'),
+    path('ChanageStatusTeacher/897<int:user_id>654/',views.ChanageStatusTeacher , name = 'ChanageStatusTeacher'),
+    path('ChanageStatusManager/897<int:user_id>654/',views.ChanageStatusManager , name = 'ChanageStatusManager'),
 
     #graph of student for manager
     path('graphStudentStatus/897<int:user_id>654/',views.graphStudentStatus, name = 'graphStudentStatus'),
